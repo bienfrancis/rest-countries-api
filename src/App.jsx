236d49fb1dfdb6128b48fa9d8be886ca684ca108
bienfrancis <bienfrancisp@gmail.com>
2023-05-07 20:21:27 +0800

    learn: state filters

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [searchQuery, setSearchQuery] = useState('');
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all')
@@ -22,13 +23,26 @@ function App() {
       })
   }, []);
 
-  const filteredData = data.filter((item) => {
-    return item.name.common.toLowerCase().includes(searchQuery.toLowerCase());
-  })
-
   const searchCountry = (event) => {
     setSearchQuery(event.target.value);
+    setFilter('');
   }
+
+  const filteredData = data.filter((item) => {
+    return (item.name.common.toLowerCase().includes(searchQuery.toLowerCase()) && item.region.includes(filter));
+  })
+
+  const filterCountry = (event) => {
+    setFilter(event.target.value);
+  }
+
+  const regions = [
+    'Africa',
+    'America',
+    'Asia',
+    'Europe',
+    'Oceania'
+]
 
   return (
     <>
@@ -37,7 +51,7 @@ function App() {
           <div className=" container py-16">
               <div className="filters flex justify-between items-center">
                 <SearchBar value={searchQuery} onChange={searchCountry}/>
-                <Filters/>
+                <Filters regions={regions} onChange={filterCountry}/>
               </div>
               <div className='my-10 grid grid-cols-4 gap-16'>
                  {
@@ -46,7 +60,7 @@ function App() {
                       img={item.flags.png}
                       alt={item.flags.alt}
                       title={item.name.common}
-                      population={item.population}
+                      population={item.population.toLocaleString()}
                       region={item.region}
                       capital={item.capital}
                     />
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 
-const Filters = () => {
+const Filters = (props) => {
 
-    const regions = [
-        'Africa',
-        'America',
-        'Asia',
-        'Europe',
-        'Oceania'
-    ]
-
+    const { regions, ...opt} = props;
+   
     return (
         <div>
-            <select name="regions" id="region" className='shadow-lg rounded-md px-6 py-4 relative'>
+            <select name="regions" id="region" className='shadow-lg rounded-md px-6 py-4 relative' {...opt} onChange={props.onChange}>
                 <option value="" disabled selected>Filter by Regions</option>
                 {
-                regions.map(region => (
-                    <option value={region} className='p-2'>{region}</option>
-                ))
+                    regions.map(region => (
+                        <option key={region} value={region} className='p-2'>{region}</option>
+                    ))
                 }
             </select>
             
